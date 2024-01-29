2b62cba3a11af27cf98022661236c28def9ad3bc
Colin LeFevre <colin.lefevre.school@gmail.com>
2024-01-29 12:44:39 -0500

    mobile navbar updates

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // Dynamically set mobile nav spacing
+    var mobileNavHeight = $("#mobile-nav").height();
+    $("#mobile-nav-spacer").height(mobileNavHeight);
+
     // Open on #hamburger-bars click
     $("#nav-mobile-bars").click(function (event) {
         if ($("#popout").hasClass("hide")) {
@@ -6,6 +10,7 @@ $(document).ready(function () {
             event.stopPropagation();
         }
     });
+
     // Close on #hamburger-x click
     $("#nav-mobile-x").click(function (event) {
         if (!$("#popout").hasClass("hide")) {
@@ -13,6 +18,7 @@ $(document).ready(function () {
             event.stopPropagation();
         }
     });
+
     // Close on external click
     $(document).click(function (event) {
         // .closest function returns array with jQuery object of closest #popout
@@ -23,4 +29,24 @@ $(document).ready(function () {
             $("#popout").addClass("hide");
         }
     });
+
+    // Hide or show mobile navbar on scroll
+    if ($(window).width() < 576) {
+        var oldScroll = $(window).scrollTop();
+        $(window).on("scroll", function () {
+            var newScroll = $(window).scrollTop();
+            // Hide on scroll down
+            if (newScroll > oldScroll && !$("#mobile-nav").hasClass("hide")) {
+                $("#mobile-nav").addClass("hide");
+            }
+            // Show on scroll up
+            else if (
+                newScroll < oldScroll &&
+                $("#mobile-nav").hasClass("hide")
+            ) {
+                $("#mobile-nav").removeClass("hide");
+            }
+            oldScroll = newScroll;
+        });
+    }
 });
